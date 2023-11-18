af1480f9302df65a28c31c0e06f85720cd523a31
Allan <allan.alves@controlle>
2023-11-18 19:35:52 -0300

    make JWT global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,11 @@ import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
-import { JwtModule } from '@nestjs/jwt';
-import { env } from './shared/config/env';
+// import { JwtModule } from '@nestjs/jwt';
+// import { env } from './shared/config/env';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      signOptions: { expiresIn: '7d' },
-      secret: env.jwtSecret,
-    }),
-    UsersModule,
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [
     {

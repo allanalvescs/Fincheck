babea654b456919edb3c4594d2029c7641ebb8bb
Allan <allan.alves@controlle>
2023-12-06 21:28:17 -0300

    feat: create service validate ownership to category module

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
+import { CategoriesService } from './service/categories.service';
 import { CategoriesController } from './categories.controller';
+import { ValidateCategoriesOwnership } from './service/validate-categories-ownership.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, ValidateCategoriesOwnership],
+  exports: [ValidateCategoriesOwnership],
 })
 export class CategoriesModule {}

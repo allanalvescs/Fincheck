38582b30c3a0ccb194f0206f5c04941d7c5092c1
Allan <allan.alves@controlle>
2023-12-10 00:18:53 -0300

    feat: create rules and validation to updated transaction

--- src/modules/transactions/service/validate-transaction-ownership.service.ts
+++ src/modules/transactions/service/validate-transaction-ownership.service.ts
@@ -1,7 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { TransactionsRepository } from 'src/shared/database/repositories/transaction.repositories';
 
 @Injectable()
 export class ValidateTransactionOwnership {
   constructor(private readonly transactionsRepo: TransactionsRepository) {}
+
+  async validate(userId: string, transactionId: string) {
+    const isOwner = await this.transactionsRepo.findFirst({
+      where: { id: transactionId, userId },
+    });
+
+    if (!isOwner) {
+      throw new UnauthorizedException('Transaction not found.');
+    }
+  }
 }
--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -3,10 +3,11 @@ import { TransactionsService } from './service/transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { BankAccountsModule } from '../bank-accounts/bank-accounts.module';
 import { CategoriesModule } from '../categories/categories.module';
+import { ValidateTransactionOwnership } from './service/validate-transaction-ownership.service';
 
 @Module({
   imports: [BankAccountsModule, CategoriesModule],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [TransactionsService, ValidateTransactionOwnership],
 })
 export class TransactionsModule {}
